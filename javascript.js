172eabdcd57e465a92da7b08681b6d7009a67904
Overforest <davidajimenezr@gmail.com>
2022-12-17 22:00:39 -0400

    Declare 'getComputerChoice()' function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+console.log("Rock, Paper, Scissors!");
+
+// A function that gets a random choice from the computer => getComputerChoice() 
+
+function getComputerChoice() {
+    const options = ['rock', 'paper', 'scissors'];
+    const choice = options[Math.floor(Math.random() * options.length + 1)];
+    return choice;
+}
+
+// A function that checks the winner => checkWinner()
+
+// A function that plays a single round => playRound()
+
+// => game()
+
